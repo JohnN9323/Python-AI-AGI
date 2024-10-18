30693d1c1acac103dad245bad13bc43a603424ce
john2393 <john1993in@gmail.com>
2024-10-18 10:16:27 +0530

    commit on 18/10/24

--- Python/Python_by_AngelaYu.py
+++ Python/Python_by_AngelaYu.py
@@ -2189,19 +2189,70 @@ MENU = {
     }
 }
 
-profit  =0 
+profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+# step1: we are trying the build self functionality that is self contain which will compare with ingredient vs choice of the drinks
+# step2: we are going to loop through order ingrediensts item and compare it with resources library items
+
+def is_resource_sufficient(order_ingredients):
+    """Return True when order can be made, False if ingredients are insufficients"""
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry ther is not enough {item}.")
+            return False
+    return True
+
+# this is function for choice of a coin
+# every subsequent ones will be added other than the first total which creates a variable and sets its value
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?:"))*0.25
+    total += int(input("how many dimes?:"))*0.1
+    total += int(input("how many nickles?:"))*0.05
+    total += int(input("how many pennies?:"))*0.01
+    return total
+
+# checking transaction successful
+# add the collect money to profit
+# if user has inserted too much monry. the machine should offer change and change will be rounded to 2 decimal places.
+
+def is_transaction_successfull(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
 is_on =True
 
 while is_on:
-   choice =input("what would you like? (espresso/latte/cappuccino):")
+   choice =input("what would you like? (espresso/latte/cappuccino): ")
    if choice == "off":
       is_on = False
 # to get the current resources values left in the machine
    elif choice == "report":
-   
+        print(f"Water: {resources['water']}m")
+        print(f"Milk: {resources['milk']}ml")      
+        print(f"Coffee:{resources['coffee']}g")
+        print(f"Money: ${profit}")
+# Check resources sufficient?
+   else:
+       drink= MENU[choice]
+       print(drink)
+       if is_resource_sufficient(drink["ingredients"]):
+          payment= process_coins()
+          is_transaction_successfull(payment, drink["cost"])
